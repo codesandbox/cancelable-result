7679bb6767680239d81d16b4618c0a8c1d575e41
Christian Alfoni <christianalfoni@gmail.com>
2021-03-24 13:00:33 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+export type Ok<V> = {
+  ok: true;
+  value: V;
+};
+
+type Err<E extends string> = {
+  ok: false;
+  error: E;
+};
+
+export type Result<V, E extends string> = Ok<V> | Err<E>;
+
+const CANCELLED_ERROR = "CANCELLED" as const;
+
+function Ok<V>(value: V): Ok<V> {
+  return {
+    ok: true,
+    value,
+  };
+}
+
+export function Err<E extends string>(error: E): Err<E> {
+  return {
+    ok: false,
+    error,
+  };
+}
+
+export function Result<V, E extends string>(
+  promise: Promise<Result<V, E | "CANCELLED">>
+): {
+  promise: Promise<Result<V, E | "CANCELLED">>;
+  cancel: () => void;
+} {
+  let isCancelled = false;
+
+  return {
+    promise: new Promise((resolve) => {
+      promise
+        .then((result) => {
+          if (!isCancelled) {
+            return result;
+          }
+
+          resolve({
+            ok: false,
+            error: CANCELLED_ERROR,
+          });
+        })
+        .catch((error) => {
+          if (!isCancelled) {
+            // This error is considered critical and will throw
+            // as normal, like syntax errors etc.
+            throw error;
+          }
+
+          resolve({
+            ok: false,
+            error: CANCELLED_ERROR,
+          });
+        });
+    }),
+    cancel: () => {
+      isCancelled = true;
+    },
+  };
+}
